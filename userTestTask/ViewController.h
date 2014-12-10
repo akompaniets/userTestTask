afae665449e683ce8f423f2aa1f6ffd6309cac9e
Andrey Kompaniets <Andrew@Mac-mini-Andrey.local>
2014-12-10 13:24:20 +0200

    Initial Commit

---
+++ userTestTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  userTestTask
+//
+//  Created by Andrey Kompaniets on 10.12.14.
+//  Copyright (c) 2014 ARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
